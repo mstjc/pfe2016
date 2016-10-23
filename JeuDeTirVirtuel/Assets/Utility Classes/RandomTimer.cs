cc8d2a30efeddbda6b9490259d5a40b62475464d
Jean-Philippe Champoux <jp33_83@hotmail.com>
2016-10-23 03:23:45 -0400

    Added first Alien.. Space key to shoot him. He moves and shoots on his own.

---
+++ JeuDeTirVirtuel/Assets/Utility Classes/RandomTimer.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomTimer {
+
+    public float MinTime = 2.0f;
+    public float MaxTime = 4.0f;
+
+    public delegate void EventHandler();
+    public event EventHandler OnTimerTick;
+
+    private float _TimeLeft;
+    public bool Started = false;
+
+    public RandomTimer(float minTime, float maxTime)
+    {
+        MinTime = minTime;
+        MaxTime = maxTime;
+    }
+
+	
+	// Update is called once per frame
+	public void Update (float delta) {
+        if(Started)
+        {
+            _TimeLeft -= delta;
+
+            if (_TimeLeft <= 0.0f)
+            {
+                if(OnTimerTick != null)
+                {
+                    OnTimerTick();
+                    Started = false;
+                }
+            }
+        }
+	}
+
+    public void StartTimer()
+    {
+        _TimeLeft = Random.Range(MinTime, MaxTime);
+        Started = true;
+    }
+
+    public void StopTimer()
+    {
+        Started = false;
+    }
+}
