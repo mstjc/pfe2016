16e032ae0918c5dccedc939e8f49b980f20c2b17
Mathieu-PC\Mathieu <w.knight.crow@gmail.com>
2016-10-23 22:17:05 -0400

    Added gun/Shield, detector to be done, We need to check together for gesture -_-

---
+++ JeuDeTirVirtuel/Assets/Script/LeapActions.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LeapActions : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject _RightHandGraphic, _LeftHandGraphic, _LeftGun, _RightGun, _LeftShield, _RightShield;
+
+    public void ActionRightHandGun(bool visibleGun)
+    {
+
+    }
+
+    public void ActionRightHandShield()
+    {
+
+    }
+
+    public void DisableLeftHandVisibleGun()
+    {
+
+    }
+
+    public void DisableLeftHandVisibleShield()
+    {
+
+    }
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ JeuDeTirVirtuel/Assets/Script/Shield.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shield : MonoBehaviour {
+
+    void Awake()
+    {
+        gameObject.SetActive(false);
+    }
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
