bba273d1be387ca83ed181789372e8f3e41128ae
Jean-Philippe Champoux <jp33_83@hotmail.com>
2016-11-09 20:05:01 -0500

    Gun animation and sound update

--- JeuDeTirVirtuel/Assets/SciFi Gun/Script/GunSoundManager.cs
+++ JeuDeTirVirtuel/Assets/SciFi Gun/Script/GunSoundManager.cs
@@ -10,10 +10,25 @@ public class GunSoundManager : MonoBehaviour {
     private AudioClip _LeftGunClip;
 
     [SerializeField]
+    private AudioClip _LeftGunReloadClip;
+
+    [SerializeField]
+    private AudioClip _LeftGunEmptyClip;
+
+    [SerializeField]
     private Gun _RightGun;
 
     [SerializeField]
     private AudioClip _RightGunClip;
+
+    [SerializeField]
+    private AudioClip _RightGunReloadClip;
+
+    [SerializeField]
+    private AudioClip _RightGunEmptyClip;
+
+    [SerializeField]
+    private float _SoundLevel = 1.0f;
 
     private float _pitchRange = 0.05f;
 
@@ -22,12 +37,17 @@ public class GunSoundManager : MonoBehaviour {
         if (_LeftGun != null)
         {
             _LeftGun.Fired += OnLeftGunFired;
+            _LeftGun.Tick += OnLeftGunTick;
+            _LeftGun.Reloaded += OnLeftGunReloaded;
         }
 
         if (_RightGun != null)
         {
             _RightGun.Fired += OnRightGunFired;
+            _RightGun.Tick += OnRightGunTick;
+            _RightGun.Reloaded += OnRightGunReloaded;
         }
+
     }
 
     void OnDisable()
@@ -35,22 +55,46 @@ public class GunSoundManager : MonoBehaviour {
         if (_LeftGun != null)
         {
             _LeftGun.Fired -= OnLeftGunFired;
+            _LeftGun.Tick -= OnLeftGunTick;
+            _LeftGun.Reloaded -= OnLeftGunReloaded;
         }
 
         if (_RightGun != null)
         {
             _RightGun.Fired -= OnRightGunFired;
+            _RightGun.Tick -= OnRightGunTick;
+            _RightGun.Reloaded -= OnRightGunReloaded;
         }
     }
 
     private void OnLeftGunFired(object sender, EventArgs e)
     {
-        PlayClipAtPoint(_LeftGunClip, _LeftGun.transform.position, 0.5f, UnityEngine.Random.Range(1 - _pitchRange, 1 + _pitchRange));
+        PlayClipAtPoint(_LeftGunClip, _LeftGun.transform.position, _SoundLevel, UnityEngine.Random.Range(1 - _pitchRange, 1 + _pitchRange));
     }
 
     private void OnRightGunFired(object sender, EventArgs e)
     {
-        PlayClipAtPoint(_RightGunClip, _RightGun.transform.position, 0.5f, UnityEngine.Random.Range(1 - _pitchRange, 1 + _pitchRange));
+        PlayClipAtPoint(_RightGunClip, _RightGun.transform.position, _SoundLevel, UnityEngine.Random.Range(1 - _pitchRange, 1 + _pitchRange));
+    }
+
+    private void OnLeftGunReloaded(object sender, EventArgs e)
+    {
+        PlayClipAtPoint(_LeftGunReloadClip, _LeftGun.transform.position, _SoundLevel, 1);
+    }
+
+    private void OnRightGunReloaded(object sender, EventArgs e)
+    {
+        PlayClipAtPoint(_RightGunReloadClip, _RightGun.transform.position, _SoundLevel, 1);
+    }
+
+    private void OnLeftGunTick(object sender, EventArgs e)
+    {
+        PlayClipAtPoint(_LeftGunEmptyClip, _LeftGun.transform.position, _SoundLevel, 1);
+    }
+
+    private void OnRightGunTick(object sender, EventArgs e)
+    {
+        PlayClipAtPoint(_RightGunEmptyClip, _RightGun.transform.position, _SoundLevel, 1);
     }
 
     // Source : https://forum.unity3d.com/threads/audiosource-pitch-question-c.164374/
@@ -67,4 +111,5 @@ public class GunSoundManager : MonoBehaviour {
         Destroy(obj, clip.length / pitch);
         return obj;
     }
+
 }
--- JeuDeTirVirtuel/Assets/Script/Gun.cs
+++ JeuDeTirVirtuel/Assets/Script/Gun.cs
@@ -1,11 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System;
+using Leap.Unity;
 
 public class Gun : MonoBehaviour {
 
     [SerializeField]
-    private GameObject _Hand;
+    private IHandModel _HandModel;
     [SerializeField]
     private Rigidbody _Shell;
     [SerializeField]
@@ -13,41 +14,76 @@ public class Gun : MonoBehaviour {
     [SerializeField]
     private float _ShellLaunchForce;
 
-    public event EventHandler Fired;
+    [SerializeField]
+    private int _NbBulletsInShell = 8;
 
-    private bool _IsReloaded = false;
+    private int _NbBullets;
+
+    public event EventHandler Fired;
+    public event EventHandler Reloaded;
+    public event EventHandler Tick;
+
+    private bool _IsReloaded = true;
+    private bool _IsReloading = false;
+    private Vector3 _OriginalRotation;
+
+    private Animator _Animator;
 
     void Awake()
     {
+        _OriginalRotation = gameObject.transform.localEulerAngles;
+        _NbBullets = _NbBulletsInShell;
+        _IsReloaded = true;
+        _Animator = GetComponent<Animator>();
         gameObject.SetActive(false);
+    }
+
+    private void OnEnable()
+    {
+    }
+
+    private void OnDisable()
+    {
     }
 
     public void SetGunVisibility(bool boolean)
     {
-        //_Hand.SetActive(!boolean);
         gameObject.SetActive(boolean);
     }
 
     public void Fire()
     {
-        if(gameObject.activeInHierarchy)
-        //if(_IsReloaded)
+        if(gameObject.activeInHierarchy && !_IsReloading && _IsReloaded)
         {
-            _IsReloaded = false;
-
             Rigidbody shellInstance = Instantiate(_Shell, _FireDirection.position, _FireDirection.rotation) as Rigidbody;
 
             shellInstance.velocity = _ShellLaunchForce * _FireDirection.forward;
 
             var particle = GetComponentInChildren<ParticleSystem>();
-            Debug.Log("particle: " + particle);
             if(particle != null)
             {
                 particle.Play();
             }
 
+            if(_Animator != null && !_IsReloading)
+            {
+                _Animator.SetTrigger("Shoot");
+            }
+
             OnFired();
 
+            // TODO
+            //_NbBullets--;
+            if (_NbBullets <= 0)
+            {
+                _IsReloaded = false;
+            }
+
+        }
+        else
+        {
+            OnTick();
+            Reload();
         }
     }
 
@@ -59,9 +95,40 @@ public class Gun : MonoBehaviour {
         }
     }
 
+    private void OnReload()
+    {
+        if (Reloaded != null)
+        {
+            Reloaded(this, EventArgs.Empty);
+        }
+    }
+
+    private void OnTick()
+    {
+        if (Tick != null)
+        {
+            Tick(this, EventArgs.Empty);
+        }
+    }
 
     public void Reload()
     {
+        lock(_Animator)
+        {
+            if(_Animator != null && !_IsReloading)
+            {
+                _IsReloading = true;
+                _Animator.SetTrigger("Reload");
+                OnReload();
+            }
+        }
+    }
+
+    public void FinishReloading()
+    {
+        Debug.Log("Finishing Reloading");
+        _NbBullets = _NbBulletsInShell;
         _IsReloaded = true;
+        _IsReloading = false;
     }
 }
