53096ae87b035bcdfab04c46a5a89d68384d9862
Jean-Philippe Champoux <jp33_83@hotmail.com>
2016-11-13 19:05:01 -0500

    Added planets (first try)

---
+++ JeuDeTirVirtuel/Assets/Script/Planet.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Planet : MonoBehaviour {
+
+    [SerializeField]
+    private Vector3 _Direction = Vector3.right;
+
+    [SerializeField]
+    private Transform _RotateAround;
+
+    [SerializeField]
+    private float _Speed = 10F;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.RotateAround(_RotateAround ? _RotateAround.transform.position : Vector3.zero, _Direction, _Speed * Time.deltaTime);
+        transform.LookAt(Vector3.zero);
+	}
+}
